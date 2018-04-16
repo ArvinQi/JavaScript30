b2cff9aea0d7b87a4d9316b088437fd5c23f4443
ArvinQi <arvin.qi@qq.com>
2018-04-16 14:20:22 +0800

    day_02

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // get post data
 const postDatas = [];
 // blog url
-const blogUrl = 'https://shunnien.github.io/';
+const blogUrl = 'http://arvinqi.com/';
 // github url
-const githubUrl = 'https://github.com/shunnien/JavaScript30day/tree/master/';
+const githubUrl = 'https://github.com/ArvinQi/JavaScript30/tree/master/';
 
 const couseList = document.querySelector('.courseList');
 
@@ -22,8 +22,7 @@ function createView() {
                 <div class="course">
                   <h2 class="course__title">第 ${data.no} 天 - ${data.title}</h2>
                   <div class="course__desc">${data.desc}</div>
-                  <a class="course__btn" href="${blogUrl}JavaScript30day/${data.github}" target="_blank" >Demo</a>
-                  <a class="course__btn" href="${blogUrl + data.blog}" target="_blank" >筆記</a>
+                  <a class="course__btn" href="${blogUrl}JavaScript30/${data.github}" target="_blank" >Demo</a>
                   <a class="course__btn" href="${githubUrl + data.github}" target="_blank">Source Code</a>
                 </div>
               </li>`);
